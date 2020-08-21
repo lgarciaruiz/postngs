904e197f18d9f5f91cb390ff1c5d0c65aa3a945d
LGarciaRuiz <lgarciaruiz23@gmail.com>
2020-08-20 22:12:42 -0700

    adding build task

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,42 @@ gulp.task('default', gulp.parallel('browser-sync','watch:sass'));
 
 /**   BUILD TASKS BELOW    **/
 
-//clean dist folder before build
+//task to delete dist folder
 gulp.task('clean', function(){
     return del(['dist']);
 });
+
+gulp.task('copyfonts', function(){
+    //select src files to copy and pipe them to their destination
+    gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+    .pipe(gulp.dest('./dist/fonts'));
+});
+
+//minification task; takes html files looks up css and js and minifies/uglifies, concatenates and replaces them by using the concatenated files in the dist folder
+gulp.task('usemin', function(){
+    //take all .html files and feed them through the pipe
+    return gulp.src('./*.html')
+    //use flatmap to feed all files at the same time
+    .pipe(flatmap(function(stream,file){
+        //pipe all three files through usemin
+        return stream
+        .pipe(usemin({
+            //makes css go through rev to get hash
+            css: [rev()],
+            //for html run it through htmlmin and remove whitespace
+            html: [function() {
+                return htmlmin({
+                    collapseWhitespace:true
+                })}],
+            //uglify js and add revision hash
+            js: [uglify(), rev()],
+            //ugflify any inline js
+            inlinejs: [uglify()],
+            inlinecss: [cleanCss(), 'concat']
+        }))
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+//specify the build task; make sure the clean task is completed first by giving it as the second param in an array
+gulp.task('build', gulp.series('clean', 'copyfonts', 'usemin'));
